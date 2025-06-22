67118e1f394c4b6df530fd43b0fa8b950fcd1b9b
Riaz Ragooloo <riazragooloo@gmail.com>
2025-06-22 13:25:27 +0200

    added initial event listeners

--- app.js
+++ app.js
@@ -9,11 +9,29 @@ class Note {                                            //creating the class
 class App {
     constructor() {
         this.notes = [];                                //creating the array to store varaibles
+
+        this.$activeForm = document.querySelector(".active-form");      //$ is a selector in JS
+        this.$activeForm.style.display = "block";
+        console.log(this.$activeForm);
+
+        this.addEventListeners();
     }
-    addNote(id, {title, text}) {                         //declaring method
+
+    addEventListeners() {
+        document.body.addEventListener("click",(event) => {
+            this.handleFormClick();
+        })
+    }
+
+    handleFormClick(event) {
+        console.log(event);
+    }
+
+    addNote(id, {title, text}) {                        //declaring method
         const newNote = new Note(id, title, text);      //creating variable and declaring parameters
         this.notes = [...this.notes, newNote]           //creating pathway to array storage
     }
+
     editNote(id, {title, text}) {
         this.notes = this.notes.map((note) => {
             if(note.id == id) {
@@ -23,6 +41,7 @@ class App {
             return note;
         });
     }
+
     displayNote(id) {
         this.notes.map(note => console.log(`
             ID: ${note.id}
@@ -30,6 +49,7 @@ class App {
             Text: ${note.text}
             `))
     }
+    
     deleteNote(id) {
         this.notes = this.notes.filter(note => note.id != id)
     }
